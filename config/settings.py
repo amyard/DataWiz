163a952d39edc71b7c5ed52f9f983c9ccd2c9326
amyard <maksymturenko@gmail.com>
2019-05-16 19:08:37 +0300

    make small changes

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+'/core'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!3mk6ybdc!h9(@&e=j$py#fh6y-bm%mp()+i4so6)onej4(!p%')
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 # SECRET_KEY = '!3mk6ybdc!h9(@&e=j$py#fh6y-bm%mp()+i4so6)onej4(!p%'
 
@@ -152,12 +152,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 
