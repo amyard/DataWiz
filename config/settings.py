d145817ed98a901dff1bf7e5750394b778925a3f
amyard <maksymturenko@gmail.com>
2019-05-16 19:11:31 +0300

    debug=False

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+'/core'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!3mk6ybdc!h9(@&e=j$py#fh6y-bm%mp()+i4so6)onej4(!p%')
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 # SECRET_KEY = '!3mk6ybdc!h9(@&e=j$py#fh6y-bm%mp()+i4so6)onej4(!p%'
 
